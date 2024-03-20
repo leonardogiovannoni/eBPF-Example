0e9c69f9032a048036609f3544a6e3112cc5b4ec
Leonardo Giovannoni <leogiovannoni@gmail.com>
2024-03-20 18:35:01 +0100

    first commit

---
+++ bpf/hello_world_bpf.c
@@ -0,0 +1,53 @@
+
+#include <uapi/linux/bpf.h>
+#include <uapi/linux/ptrace.h>
+#include <uapi/linux/if_ether.h>
+#define __attribute_const__ __attribute__((const))
+#include <uapi/linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#include <stddef.h>
+
+#include <uapi/linux/bpf.h>
+#include <uapi/linux/ptrace.h>
+#include <uapi/linux/if_ether.h>
+#include <uapi/linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#include <stddef.h>
+
+#define PTR_AT(ctx, offset, type) \
+    (((void *)(long)(ctx)->data + (offset) + sizeof(type) <= (void *)(long)(ctx)->data_end) ? \
+    (type *)((void *)(long)(ctx)->data + (offset)) : \
+    (type *)(NULL))
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(key_size, sizeof(u32));
+    __uint(value_size, sizeof(u32));
+    __uint(max_entries, 256);
+} EVENTS SEC(".maps");
+
+SEC("xdp")
+int get_ip_source(struct xdp_md *ctx) {
+    struct ethhdr *ethhdr = PTR_AT(ctx, 0, struct ethhdr);
+
+    if (!ethhdr)
+        return XDP_ABORTED;
+
+    if (ethhdr->h_proto != __constant_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    struct iphdr *iphdr = PTR_AT(ctx, sizeof(*ethhdr), struct iphdr);
+    if (!iphdr)
+        return XDP_ABORTED;
+
+    struct iphdr iph = *iphdr;
+    bpf_perf_event_output(ctx, &EVENTS, BPF_F_CURRENT_CPU, &iph, sizeof(iph));
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
+
